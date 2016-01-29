3164bb6e07433163067104a9e0fab4d78a010d8b
Pavel Kvas <kvaspasha@yandex.ru>
2016-01-29 14:08:38 +0200

    add GameManager script
    add round time indicator

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float RoundLength = 10f;
+    private float curRoundTimeLeft;
+    private int score = 0;
+    private bool isPlaying = true;
+    
+    public Slider RoundTimeIndicator;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    curRoundTimeLeft = RoundLength;
+	    RoundTimeIndicator.maxValue = RoundLength;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (!isPlaying) return;
+
+	    curRoundTimeLeft -= Time.deltaTime;
+	    RoundTimeIndicator.value = curRoundTimeLeft;
+	    if (curRoundTimeLeft < 0)
+	    {
+	        Debug.Log(string.Format("Time elapsed, your score is {0} points", score));
+	        isPlaying = false;
+	    }
+	}
+}
