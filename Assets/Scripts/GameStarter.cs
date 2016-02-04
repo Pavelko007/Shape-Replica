9ae4db245cb73590b1cfd1f9b69b7f4a0ab96f6e
Pavel Kvas <kvaspasha@yandex.ru>
2016-02-04 17:22:17 +0200

    add begin scene

---
+++ Assets/Scripts/GameStarter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
