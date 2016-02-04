9ada9890bf14c9d6ba81c8f0e16cbdfeaa2dda49
Pavel Kvas <kvaspasha@yandex.ru>
2016-02-04 17:08:02 +0200

    replace OnGUI with new UI in GestureEditor

--- Assets/Scripts/GestureEditor.cs
+++ Assets/Scripts/GestureEditor.cs
@@ -1,51 +1,35 @@
 using System;
 using PDollarGestureRecognizer;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ShapeReplica
 {
     class GestureEditor : MonoBehaviour
     {
         [SerializeField] private DrawingBoard drawingBoard;
+        [SerializeField] private InputField inputField;
 
-        private string newGestureName = "";
-        
-        protected void ProcessAddNewGesture()
+        private string shapeTitle = "";
+
+        public void SetShapeTitle(string newTitle)
         {
-            var gestureNameLabelRect = new Rect(Screen.width - 200, 150, 70, 30);
-            GUI.Label(gestureNameLabelRect, "Add as: ");
-
-            var gestureNameTextFieldRect = new Rect(Screen.width - 150, 150, 100, 30);
-            newGestureName = GUI.TextField(gestureNameTextFieldRect, newGestureName);
-
-            var addGestureRect = new Rect(Screen.width - 50, 150, 50, 30);
-
-            if (GUI.Button(addGestureRect, "Add") &&
-                drawingBoard.DrawingPoints.Count > 0 &&
-                newGestureName != "")
-            {
-                AddNewGesture();
-            }
+            shapeTitle = newTitle;
         }
 
-        private void AddNewGesture()
+        public void AddNewGesture()
         {
+            if (drawingBoard.DrawingPoints.Count <= 0 || shapeTitle == "") return;
+
             string fileName = string.Format("{0}/{1}-{2}.xml",
                 Application.persistentDataPath,
-                newGestureName,
+                shapeTitle,
                 DateTime.Now.ToFileTime());
 
 #if !UNITY_WEBPLAYER
-            GestureIO.WriteGesture(drawingBoard.DrawingPoints.ToArray(), newGestureName, fileName);
+            GestureIO.WriteGesture(drawingBoard.DrawingPoints.ToArray(), shapeTitle, fileName);
 #endif
-            newGestureName = "";
+            inputField.text = "";
         }
-
-        void OnGUI()
-        {
-            ProcessAddNewGesture();
-        }
-
-      
     }
 }
