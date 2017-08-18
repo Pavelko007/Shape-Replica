17eee8ec8c592bec47154cbe57f263dabb355234
Pavel Kvas <kvaspasha@yandex.ua>
2017-08-18 16:55:21 +0300

    add menu item for opening main scene; rename scenes

---
+++ Assets/Scripts/Editor/OpenMainSceneEditorMenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace ShapeReplica.Editor
+{
+    public class OpenMainSceneEditorMenuItem : MonoBehaviour
+    {
+        [MenuItem("Assets/Open Main Scene %m", false, 0)]
+        private static void OpenMainScene()
+        {
+            var mainSceneLabel = "MainScene";
+            var mainSceneGUID = AssetDatabase.FindAssets(string.Format("l:{0}", mainSceneLabel)).First();
+            var assetPath = AssetDatabase.GUIDToAssetPath(mainSceneGUID);
+            EditorSceneManager.OpenScene(assetPath, OpenSceneMode.Single);
+        }
+    }
+}
