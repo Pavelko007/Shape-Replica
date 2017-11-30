d80ac5215423b508717403512f88522eb397637e
Pavlo Kvas <kvaspasha@gmail.com>
2017-11-30 22:57:59 +0200

    create a bool event

---
+++ Assets/Scripts/EventSystem/GameEventBool.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShapeReplica.EventSystem
+{
+    [CreateAssetMenu(fileName = "NewGameEventBool.asset", menuName = "GameEventBool", order = 1)]
+    public class GameEventBool : ScriptableObject
+    {
+        private List<GameEventListenerBool> listeners = new List<GameEventListenerBool>();
+
+        public void Raise(bool value)
+        {
+            for (int i = listeners.Count - 1; i >= 0; i--)
+            {
+                listeners[i].OnEventRaised(value);
+            }
+        }
+
+        public void RegisterListener(GameEventListenerBool listener)
+        {
+            listeners.Add(listener);
+        }
+
+        public void UnregisterListener(GameEventListenerBool listener)
+        {
+            listeners.Remove(listener);
+        }
+    }
+}
---
+++ Assets/Scripts/EventSystem/GameEventListenerBool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ShapeReplica.EventSystem
+{
+    [Serializable]
+    public class UnityEventBool : UnityEvent<bool> { }
+
+    public class GameEventListenerBool : MonoBehaviour
+    {
+        public GameEventBool Event;
+        public UnityEventBool Response; 
+
+        private void OnEnable()
+        {
+            Event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            Event.UnregisterListener(this);
+        }
+
+        public void OnEventRaised(bool value)
+        {
+            Response.Invoke(value);
+        }
+    }
+}
