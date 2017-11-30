546716d4b269d6e8ad0e81415acbfdd8152ebd16
Pavlo Kvas <kvaspasha@gmail.com>
2017-11-30 15:31:56 +0200

    create new event system

---
+++ Assets/Scripts/EventSystem/GameEventListener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace ShapeReplica.EventSystem
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        public GameEvent Event;
+        public UnityEvent Response;//todo extend for arguments
+
+        private void OnEnable()
+        {
+            Event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            Event.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            Response.Invoke();
+        }
+    }
+}
