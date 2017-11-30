7faa0c03bc7a245286203c2ce7330fb7756793ad
Pavlo Kvas <kvaspasha@gmail.com>
2017-11-30 19:01:31 +0200

    add OnQuitGame event

--- Assets/Scripts/Events/EventCollection.cs
+++ Assets/Scripts/Events/EventCollection.cs
@@ -3,7 +3,5 @@ namespace ShapeReplica
     public static class EventCollection
     {
         public const string OpenMenu = "menu button clicked";
-        public const string Quit = "quit";
-        public const string Pause = "pause";
     }
 }
